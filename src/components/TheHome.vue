64cd948e514a023906354b34ca212871d0a74e5e
junior-git <jddb26@gmail.com>
2020-12-12 20:34:59 -0500

    funcional se incorporo el menu

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -46,7 +46,55 @@
 
 
 
+<div>
+    <top-menu></top-menu>
 
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1" class=""></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2" class=""></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/Banner1.jpg" class="d-block w-100 " alt="Banner1" style="object-fit:cover;" height="300">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Las noticias mas actualizadas</h5>
+                    <p>Continúa y conoce cuales son las noticias más recientes del momento.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/Banner2.jpg" class="d-block w-100" alt="Banner2" style="object-fit:cover;" height="300">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Los últimos avances en salud</h5>
+                    <p>Sabemos que la salud nos interesa a todos.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/Banner3.jpeg" class="d-block w-100" alt="Banner3" style="object-fit:cover;" height="300">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Las novedades tecnólogicas</h5>
+                    <p>Porque la tecnología no se detiene.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+
+
+
+
+
+    <!-- ]seccion  api -->
     <div  id="new" class="container-fluid">
       <h2 class="text-center pt-5 mb-3">NOTICIAS</h2>
 
@@ -55,7 +103,7 @@
 
 
 
-    
+    <!-- secccion cards -->
     <div id="equipo" class="container-fluid  pb-5  pt-2">
       <h2 class="text-center mt-5 pb-5">INTEGRANTES EQUIPO 101 MISIÓN TIC</h2>
       <div class="card-group pt-3 ">
@@ -75,14 +123,9 @@
 
 
 
-
-
-
-
-
    
 
-
+    <!-- seccion footer -->
     <div class="container-fluid pt-5">
       <page-footer></page-footer>
     </div>
@@ -104,7 +147,7 @@ import TopBanner from "./TopBanner.vue";
 export default {
 
 components: {
-    // TopBanner,
+    TopBanner,
     TeamCard,
     SectionApi,
     PageFooter,
