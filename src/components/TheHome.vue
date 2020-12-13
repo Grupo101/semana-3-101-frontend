41ae3ac3bd0fdec9fa419b31ed964fc680bf6760
junior-git <jddb26@gmail.com>
2020-12-12 20:25:04 -0500

    uncional agrego las cards

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -45,6 +45,16 @@
     </div>
 
 
+
+
+    <div  id="new" class="container-fluid">
+      <h2 class="text-center pt-5 mb-3">NOTICIAS</h2>
+
+      <section-api> </section-api>
+    </div>
+
+
+
     
     <div id="equipo" class="container-fluid  pb-5  pt-2">
       <h2 class="text-center mt-5 pb-5">INTEGRANTES EQUIPO 101 MISIÓN TIC</h2>
@@ -70,11 +80,7 @@
 
 
 
-    <div  id="new" class="container-fluid">
-      <h2 class="text-center pt-5 mb-3">NOTICIAS</h2>
-
-      <section-api> </section-api>
-    </div>
+   
 
 
     <div class="container-fluid pt-5">
@@ -93,7 +99,7 @@
 import TeamCard from "./TeamCard.vue";
 import PageFooter from "./PageFooter.vue";
 import SectionApi from "./SectionApi.vue";
-//import TopBanner from "./components/TopBanner.vue";
+import TopBanner from "./TopBanner.vue";
 
 export default {
 
@@ -109,6 +115,48 @@ components: {
   data() {
     return {
       user: {},
+       member: [
+        {
+          codigo: "1",
+          nombre: "Nelly Alexandra Medina Rojas",
+          descripcion:
+            "Ingeniera Electronica de la Escuela Colombiana de ingenieria Julio Garavito con experiencia en infraestructuras de LANs, backbone, Wi-Fi y diseño. Entre sus pasatiempos esta ver peliculas, salir a dar paseos y leer.",
+          rol: "Arquitecta de Software",
+          image: "./alexandra.png",
+        },
+        {
+          codigo: "2",
+          nombre: "José Daniel Delgado Ballen",
+          descripcion:
+            "Ingeniero de sistemas de la Universidad de los Libertadores con experiencia en el area de comunicaciones electromagneticas. Entre sus pasatiempos esta resolver crucigramas y sudokus.",
+          rol: "Desarrollador Frontend",
+          image: "./jose.png",
+        },
+        {
+          codigo: "3",
+          nombre: "Jimmy Javier Piamonte Cañón",
+          descripcion:
+            "Tecnologo en sistemas de la informacion de la Universidad del Tolima con experiencia en desarrollo .NET, web y escritorio. Entre sus pasatiempo esta montar bicicleta.",
+          rol: "Desarrollador Backend",
+          image: "./jimmy.png",
+        },
+        {
+          codigo: "4",
+          nombre: "Juan Sebastian Loaiza Muñoz",
+          descripcion:
+            "Ingeniero quimico de la Univesidad Nacional de Colombia. Docente de quimica y matematicas de bachillerato. Hobbies: literatura y series de televisión",
+          rol: "Tester",
+          image: "./sebastian.png",
+        },
+        {
+          codigo: "5",
+          nombre: "Joaquin Andres Alarcon Guevara",
+          descripcion:
+            "Ingeniero electronico de la Univesidad del Valle. Coordinador de investigacion y desarrollo en IGT S.A.S. Hobbies: video juegos",
+          rol: "Analista de Software",
+          image: "./joaquin.png",
+        },
+      ],
     };
   },
   methods: {
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark ">
+        <a class="navbar-brand " href="# ">
+            <img src="../assets/LogoTecSalud.jpg" width="40 " height="40
+        " class="d-inline-block align-center " alt=" " loading="lazy "> TECNOLOGÍA Y SALUD
+        </a>
+        <div class="collapse navbar-collapse justify-content-end " id="navbarNav ">
+            <ul class="navbar-nav ">
+                <li class="nav-item active ">
+                    <a class="nav-link " href="# ">Home <span class="sr-only ">(current)</span></a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="#new">News</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="#equipo">Team</a>
+                </li>                
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+  name: "TopMenu",
+};
+</script>
+
+<style scoped>
+</style>
